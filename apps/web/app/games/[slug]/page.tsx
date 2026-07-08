be4959d0f3ab3692f2691c6b4ae7badc5b56d71b
ricardocesidio <158211406+ricardocesidio@users.noreply.github.com>
2026-07-08 14:10:11 +0100

    fix: pull content grid up 50px

--- apps/web/app/games/[slug]/page.tsx
+++ apps/web/app/games/[slug]/page.tsx
@@ -174,7 +174,7 @@ function PremiumGameDetail({
 
           <TagRow tags={tags} />
 
-          <section className="grid items-start gap-4 xl:grid-cols-[1fr_430px]">
+          <section className="grid items-start gap-4 xl:grid-cols-[1fr_430px] -mt-[50px]">
             <div className="grid gap-4">
               <div className="grid items-start gap-4 lg:grid-cols-[0.95fr_1.05fr]">
                 <TrailerPanel title={title} image={heroImage} trailerUrl={game.trailerUrl} />
