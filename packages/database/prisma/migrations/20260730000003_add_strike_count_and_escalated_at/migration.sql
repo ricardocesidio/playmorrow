0edfc2594493c61ddfcace0669fba22ddbe08a40
Ricardo Cesídio <158211406+ricardocesidio@users.noreply.github.com>
2026-07-30 15:20:35 +0100

    FIX tests: add missing lastStrikeAt to migration (companion to strikeCount, also missing)

--- packages/database/prisma/migrations/20260730000003_add_strike_count_and_escalated_at/migration.sql
+++ packages/database/prisma/migrations/20260730000003_add_strike_count_and_escalated_at/migration.sql
@@ -3,3 +3,6 @@ ALTER TABLE "users" ADD COLUMN "strikeCount" INTEGER NOT NULL DEFAULT 0;
 
 -- AlterTable: Add escalatedAt to ModerationReport
 ALTER TABLE "moderation_reports" ADD COLUMN "escalatedAt" TIMESTAMP(3);
+
+-- AlterTable: Add lastStrikeAt to User (companion to strikeCount)
+ALTER TABLE "users" ADD COLUMN "lastStrikeAt" TIMESTAMP(3);
